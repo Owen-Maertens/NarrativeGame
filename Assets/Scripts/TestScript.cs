31147b20cf1e5e538a89c8d0ae29c5e6dce7189a
Owen Maertens <owenmaertens45@gmail.com>
2020-03-20 15:11:14 -0400

    Initialized Project
    Setup Project and imported fungus

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //comment
+        transform.position += new Vector3(1,0,0)*Time.deltaTime;
+    }
+}
